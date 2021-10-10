7f6f6cb9776ff4d4131bd40c6e4338f55aa1e8a3
Vallie Tracy <trac0055@umn.edu>
2021-10-10 07:36:31 -0500

    made first three classes in OO portion of study guide

--- oo.py
+++ oo.py
@@ -1,1 +1,17 @@
 # Create your classes here
+
+class Student:
+    def __init__(self, first_name, last_name, address):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+
+class Question:
+    def __init__(self, question, correct_answer):
+        self.question = question
+        self.correct_answer = correct_answer
+
+class Exam:
+    def __init__(self, name, questions):
+        self.name = name
+        self.questions = []
